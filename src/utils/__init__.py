d8d613ae76cc9285de099abd75fbc57262e90ddd
Michael Paglia <michaelpaglia02@gmail.com>
2026-01-27 23:08:50 -0500

    feat: add Twitter API integration for real sentiment data

---
+++ src/utils/__init__.py
@@ -0,0 +1,5 @@
+"""Utility functions."""
+
+from .twitter import TwitterClient, TweetData, SentimentPrior, get_market_sentiment
+
+__all__ = ["TwitterClient", "TweetData", "SentimentPrior", "get_market_sentiment"]
